57fed35c66e21a70d3a67f1930c7f11b6095ff85
Samat Galimov <git@samat.me>
2019-06-11 18:18:53 +0100

    remove private key

--- src/funcs/send-inline-answer.php
+++ src/funcs/send-inline-answer.php
@@ -4,7 +4,7 @@ use \Curl\Curl;
 
 function sendInlineAnswer($id, $results) {
   $curl = new Curl();
-  $curl->post('https://api.telegram.org/***REMOVED***/answerInlineQuery', [
+  $curl->post('https://api.telegram.org/your_key/answerInlineQuery', [
     'inline_query_id' => $id,
     'results' => json_encode($results),
     'cache_time' => 6000,
--- src/funcs/send-message.php
+++ src/funcs/send-message.php
@@ -4,7 +4,7 @@ use \Curl\Curl;
 
 function sendMessage($req, $text) {
   $curl = new Curl();
-  $curl->post('https://api.telegram.org/***REMOVED***/sendMessage', [
+  $curl->post('https://api.telegram.org/your_key/sendMessage', [
     'chat_id' => $req->message->chat->id,
     'text' => $text,
     'parse_mode' => 'markdown',
